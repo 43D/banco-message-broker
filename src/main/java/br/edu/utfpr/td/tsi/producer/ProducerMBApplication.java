dbd6f8caad470a8c00575a02c61deb5c249223ce
Allan <allan.goioere@hotmail.com>
2023-11-21 15:23:20 -0300

    Start project

---
+++ src/main/java/br/edu/utfpr/td/tsi/producer/ProducerMBApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.td.tsi.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerMBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerMBApplication.class, args);
+	}
+
+}
