363c10c457893bdffbbb6c37bc33cdc9cca80b15
Allan <allan.goioere@hotmail.com>
2023-11-21 15:26:07 -0300

    start project

---
+++ src/main/java/br/edu/utfpr/td/tsi/consumer/ConsumerMBApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.td.tsi.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerMBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerMBApplication.class, args);
+	}
+
+}
