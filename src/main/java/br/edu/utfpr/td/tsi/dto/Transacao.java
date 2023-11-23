ba8736840261e8461290dec548aaab86d93a67e5
Allan <allan.goioere@hotmail.com>
2023-11-23 20:37:25 -0300

    DTO transacao

---
+++ src/main/java/br/edu/utfpr/td/tsi/dto/Transacao.java
@@ -0,0 +1,40 @@
+package br.edu.utfpr.td.tsi.dto;
+
+import java.util.Date;
+
+public class Transacao {
+    private long id;
+    private String credente;
+    private String pagador;
+    private double valor;
+    private Date vencimento;
+
+    public Transacao(long id, String credente, String pagador, double valor, Date vencimento) {
+        this.id = id;
+        this.credente = credente;
+        this.pagador = pagador;
+        this.valor = valor;
+        this.vencimento = vencimento;
+    }
+
+    public long getId() {
+        return id;
+    }
+    
+    public String getCredente() {
+        return credente;
+    }
+    
+    public String getPagador() {
+        return pagador;
+    }
+    
+    public double getValor() {
+        return valor;
+    }
+    
+    public Date getVencimento() {
+        return vencimento;
+    }
+
+}
